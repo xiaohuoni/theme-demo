35c0b60ad6af297a07262cea22b7363a6c180a02
xiaohuoni <xiaohuoni@dingtalk.com>
2023-07-31 11:23:38 +0800

    feat: less modify vars

---
+++ examples/antd-theme-less/config/config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'alita';
+
+import MergeLessPlugin from 'antd-pro-merge-less';
+import AntDesignThemePlugin from 'antd-theme-webpack-plugin';
+import path from 'path';
+
+console.log(MergeLessPlugin)
+export default defineConfig({
+  appType: 'pc',
+  // 这个值在 appts 中被 getKeepAlive 修改
+  keepalive: [/./],
+  antd:{},
+  chainWebpack:(config)=>{
+    if (
+      process.env.NODE_ENV === 'production'
+    ) {
+      // 将所有 less 合并为一个供 themePlugin使用
+      const outFile = path.join(__dirname, '../.temp/merge.less');
+      const stylesDir = path.join(__dirname, '../src/');
+  
+      config.plugin('merge-less').use(new MergeLessPlugin(), [
+        {
+          stylesDir,
+          outFile,
+        },
+      ]);
+  
+      config.plugin('ant-design-theme').use(AntDesignThemePlugin, [
+        {
+          antDir: path.join(__dirname, '../node_modules/antd'),
+          stylesDir,
+          varFile: path.join(__dirname, '../node_modules/antd/lib/style/themes/default.less'),
+          mainLessFile: outFile,
+          // themeVariables: ['@primary-color',],
+          indexFileName: 'index.html',
+          generateOne: true,
+          lessUrl: 'https://gw.alipayobjects.com/os/lib/less.js/3.8.1/less.min.js',
+        },
+      ]);
+    }
+  }
+});
---
+++ examples/antd-theme-less/src/pages/index/index.tsx
@@ -0,0 +1,75 @@
+import { Button, ConfigProvider } from 'antd';
+
+import { useState } from 'react';
+let lessNodesAppended = false;
+
+export default () => {
+  const [count, setCount] = useState(0);
+  return (
+    <ConfigProvider>
+      <Button
+        type="primary"
+        block
+        size="large"
+        onClick={() => {
+          function buildIt() {
+            if (!window.less) {
+              return;
+            }
+            setTimeout(() => {
+              window.less
+                .modifyVars({
+                  '@primary-color': 'red',
+                })
+            }, 200);
+          }
+          if (!lessNodesAppended) {
+            // insert less.js and color.less
+            const lessStyleNode = document.createElement('link');
+            const lessConfigNode = document.createElement('script');
+            const lessScriptNode = document.createElement('script');
+            lessStyleNode.setAttribute('rel', 'stylesheet/less');
+            lessStyleNode.setAttribute('href', '/color.less');
+            lessConfigNode.innerHTML = `
+              window.less = {
+                async: true,
+                env: 'production',
+                javascriptEnabled: true
+              };
+            `;
+            lessScriptNode.src =
+              'https://gw.alipayobjects.com/os/lib/less.js/3.8.1/less.min.js';
+            lessScriptNode.async = true;
+            lessScriptNode.onload = () => {
+              buildIt();
+              lessScriptNode.onload = null;
+            };
+            document.body.appendChild(lessStyleNode);
+            document.body.appendChild(lessConfigNode);
+            document.body.appendChild(lessScriptNode);
+            lessNodesAppended = true;
+          } else {
+            buildIt();
+          }
+        }}
+      >
+        切换
+      </Button>
+      <div>
+        <h1>antd</h1>
+        <div>
+          {Math.random()}
+          <Button
+            type="primary"
+            className="purple-theme"
+            block
+            size="large"
+            onClick={() => setCount(count + 1)}
+          >
+            点我计数加1134 {count}
+          </Button>
+        </div>
+      </div>
+    </ConfigProvider>
+  );
+};
